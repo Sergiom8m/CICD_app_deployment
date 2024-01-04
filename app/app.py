e54cf69fe8228c6464bcd1ae453868acbe3a34d1
Sergio Martín <81020872+Sergiom8m@users.noreply.github.com>
2024-01-04 16:24:26 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -17,4 +17,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hola! Este sitio se ha visitado {} veces.\n'.format(count)
+    return 'Hola! Este sitio se ha visitado {} veces. GACIAS POR ESTAR AQUI!!\n'.format(count)
